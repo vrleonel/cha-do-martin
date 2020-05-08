6fff1cac368f04393afd8762f58085a5f82ceced
Vitor Leonel <vitor.leonel@loggi.com>
2020-05-08 15:01:44 -0300

    add eslint config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   siteMetadata: {
     title: `Chá do Martin`,
     description: `Chá de bebê on-line do Martin. Mande seu carinho virtual. =D`,
-    author: `@gatsbyjs`,
+    author: `@vrleonel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,6 +19,17 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-sass`,
     {
+      resolve: 'gatsby-plugin-eslint-v2',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|cache|public)/,
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -29,7 +41,35 @@ module.exports = {
         icon: `src/images/raposinha.png`, // This path is relative to the root of the site.
       },
     },
-    
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-165929856-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "chadomartin.leonel.dev",
+      },
+    },
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
@@ -38,6 +78,7 @@ module.exports = {
         }
       }
     },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
